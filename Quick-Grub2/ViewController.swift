20e09a78ba29c2ee2118d40e8dd9e29b9d20e9cf
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-19 09:23:12 -0500

    Iniital commit. removes all reference to storyboard

---
+++ Quick-Grub2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick-Grub2
+//
+//  Created by Mr Wonderful on 11/19/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quick-Grub2Tests/Quick_Grub2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Quick_Grub2Tests.swift
+//  Quick-Grub2Tests
+//
+//  Created by Mr Wonderful on 11/19/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import XCTest
+@testable import Quick_Grub2
+
+class Quick_Grub2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
