9a3f51d6eb9fed0e97554eb502ffeff3b28f7a9a
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-19 09:36:29 -0500

    Adds UIColor-Extension file to project

---
+++ Quick-Grub2/UIColor-Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor-Extension.swift
+//  Quick-Grub2
+//
+//  Created by Mr Wonderful on 11/19/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static func colorWithRedValue(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
+    }
+}
