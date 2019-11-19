fdc99355ae092363f85a41a1ebfaa78eaae734a0
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-19 15:22:43 -0500

    Creates a CategoryCollectionViewCell file

---
+++ Views/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Quick-Grub2
+//
+//  Created by Mr Wonderful on 11/19/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
