786d0ee97fecadc79fae51bde69587bb7ad0dab6
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-19 15:46:28 -0500

    Adds git status

--- Views/CategoryCollectionViewCell.swift
+++ Views/CategoryCollectionViewCell.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
     
+    lazy var checkMarkButton:UIButton = {
+       let button = UIButton()
+        return button
+    }()
+    
+    lazy var catrgoryLabel:UILabel = {
+        let label = UILabel(textAlignment: .center, text: "")
+        return label
+    }()
+    
+    lazy var categoryImage:UIImageView = {
+    let image = UIImageView()
+        return image
+    }()
+    
+    override init (frame:CGRect){
+           super.init(frame:frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
+
