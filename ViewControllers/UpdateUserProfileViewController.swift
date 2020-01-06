be3b0ecc452e8b3c46c68a224c36a6f0c2923b27
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.fios-router.home>
2020-01-05 22:16:33 -0500

    Creates topView and constraints the view to the superview

--- ViewControllers/UpdateUserProfileViewController.swift
+++ ViewControllers/UpdateUserProfileViewController.swift
@@ -9,22 +9,81 @@
 import UIKit
 
 class UpdateUserProfileViewController: UIViewController {
-
+    //MARK: UI Objects
+    lazy var updateProfilrLate:UILabel = {
+        let label = UILabel()
+        label.text = "Update Profile"
+        return label
+    }()
+    
+    lazy var profileImage:UIImageView = {
+        let guesture = UITapGestureRecognizer(target: self, action: #selector(imageViewDoubleTapped(sender:)))
+        guesture.numberOfTapsRequired = 1
+        let image = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        image.layer.cornerRadius = image.frame.height / 2
+        image.clipsToBounds = true
+        image.image = UIImage(named: "profileImage")
+        image.isUserInteractionEnabled = true
+        image.contentMode = .scaleAspectFill
+        image.layer.borderWidth = 2
+        image.layer.borderColor = UIColor.white.cgColor
+        image.addGestureRecognizer(guesture)
+        return image
+    }()
+    
+    lazy var topView:UIView = {
+       let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    lazy var rightBarButton:UIBarButtonItem = {
+        let settingsImage = UIImage(systemName: "gear")
+        let button = UIBarButtonItem(image: settingsImage, style: .plain, target: self, action: #selector(handleSettingsButtonPressed(_:)))
+          
+           button.tintColor = .black
+           return button
+       }()
+    
+    lazy var updateButton:UIButton = {
+        let button = UIButton()
+        button.setTitle("Update", for: .normal)
+        button.titleLabel?.font =  UIFont(name: "HelveticaNeue-Bold", size: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(handleUpdateButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        configureNavigationBar()
+        configureTopViewConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: @objc function
+    @objc func handleUpdateButtonPressed(){
+        print("update button pressed")
     }
-    */
-
+    
+    @objc func imageViewDoubleTapped(sender:UITapGestureRecognizer){
+        print("Image view tapped")
+    }
+    
+    @objc func handleSettingsButtonPressed(_ sender:UIBarButtonItem){
+        print("Image view tapped")
+    }
+    private func configureNavigationBar(){
+        navigationController?.navigationBar.barTintColor = .white
+        self.navigationItem.rightBarButtonItem = rightBarButton
+    }
+    
+    private func configureTopViewConstraints(){
+        view.addSubview(topView)
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),topView.leadingAnchor.constraint(equalTo: view.leadingAnchor), topView.trailingAnchor.constraint(equalTo: view.trailingAnchor), topView.heightAnchor.constraint(equalToConstant: 140)])
+    }
+    
 }
