be35773c7f0ee19c39c9b029fcb967faa3cc597f
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.fios-router.home>
2020-01-11 13:38:12 -0500

    Adds ForgetPasswordViewController to project

---
+++ ViewControllers/ForgetPasswordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ForgetPasswordViewController.swift
+//  Quick-Grub2
+//
+//  Created by Mr Wonderful on 1/11/20.
+//  Copyright © 2020 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class ForgetPasswordViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
