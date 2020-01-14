dbab876f7db2c4eb707898d680400d1fa29a6502
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2020-01-14 16:51:30 -0500

    Adds custom tableview to FavoriteViewController

--- ViewControllers/FavoriteViewController.swift
+++ ViewControllers/FavoriteViewController.swift
@@ -9,16 +9,82 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
-
-//    lazy var tableView = UITableView{
-//        
-//        }()
+    
+    
+    var businessFullDetail = [CDYelpBusiness](){
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    
+    enum Const {
+        static let closeCellHeight: CGFloat = 179
+        static let openCellHeight: CGFloat = 488
+        static let rowsCount = 50
+    }
+    
+    lazy var tableView:UITableView = {
+        let tableview = UITableView()
+        tableview.register(FoldingCell.self, forCellReuseIdentifier: ResturantCellIdentifier.resturantCell.rawValue)
+        return tableview
+    }()
+    
+    var cellHeights: [CGFloat] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDelegation()
+        configureTableViewConstraints()
+        setup()
     }
-
-
+    
+    
+    private func setDelegation(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    // MARK: Helpers
+    private func setup() {
+        cellHeights = Array(repeating: Const.closeCellHeight, count: Const.rowsCount)
+        tableView.estimatedRowHeight = Const.closeCellHeight
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "background"))
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = UIRefreshControl()
+            tableView.refreshControl?.addTarget(self, action: #selector(refreshHandler), for: .valueChanged)
+        }
+    }
+    
+    // MARK: Actions
+    @objc func refreshHandler() {
+        let deadlineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { [weak self] in
+            if #available(iOS 10.0, *) {
+                self?.tableView.refreshControl?.endRefreshing()
+            }
+            self?.tableView.reloadData()
+        })
+    }
+    
+    
+    
+    func configureTableViewConstraints(){
+        self.view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([tableView.topAnchor.constraint(equalTo: self.view.topAnchor), tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor), tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)])
+        
+    }
+}
+extension FavoriteViewController: UITableViewDelegate{}
+extension FavoriteViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
