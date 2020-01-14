2adbcc236403b75d1628cc82983f71e178fe25eb
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2020-01-14 14:56:23 -0500

    Adds FavoriteViewController to project

---
+++ ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteViewController.swift
+//  Quick-Grub2
+//
+//  Created by Mr Wonderful on 1/14/20.
+//  Copyright © 2020 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+//    lazy var tableView = UITableView{
+//        
+//        }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
