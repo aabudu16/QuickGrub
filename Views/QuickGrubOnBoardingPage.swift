b301bc5a454500b02f2d48e35df1e0605d9881c9
aabudu16 <Aabudu16@yahoo.com>
2020-02-19 16:03:55 -0500

    Adds title label, suTitle label and imageView to QuickGrubOnBoardingPage.swift file with constraints

--- Views/QuickGrubOnBoardingPage.swift
+++ Views/QuickGrubOnBoardingPage.swift
@@ -1,9 +1,60 @@
-//
-//  QuickGrubOnBoardingPage.swift
-//  Quick-Grub2
-//
-//  Created by Mr Wonderful on 2/19/20.
-//  Copyright © 2020 Mr Wonderful. All rights reserved.
-//
+import UIKit
 
-import Foundation
+open class QuickGrubOnBoardingPage: UIView {
+    
+    public var title: UILabel = {
+        let label = UILabel()
+        label.text = "Page Title"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.sizeToFit()
+        return label
+    }()
+    
+    public var subTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Page Sub Title"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.sizeToFit()
+        return label
+    }()
+    
+    public var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .gray
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        imageViewConstraint()
+        titleConstraint()
+        subViewConstraint()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func imageViewConstraint() {
+        self.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 30), imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30), imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30), imageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.5)])
+    }
+    
+    private func titleConstraint() {
+        self.addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([title.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10), title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30), title.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30), title.heightAnchor.constraint(equalToConstant: 50)])
+    }
+    
+    private func subViewConstraint() {
+        self.addSubview(subTitle)
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([subTitle.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 5), subTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30), subTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30), subTitle.heightAnchor.constraint(equalToConstant: 100)])
+    }
+}
