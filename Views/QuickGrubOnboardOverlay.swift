34f0dee3029fe087c1781dd6b4f17f27fe551b91
aabudu16 <Aabudu16@yahoo.com>
2020-02-19 13:46:10 -0500

    Adds pageControl, continueButton, skipButton, prevLabel, nextLabel to QuickGrubOnboardOverlay.swift

--- Views/QuickGrubOnboardOverlay.swift
+++ Views/QuickGrubOnboardOverlay.swift
@@ -1,9 +1,54 @@
-//
-//  QuickGrubOnboardOverlay.swift
-//  Quick-Grub2
-//
-//  Created by Mr Wonderful on 2/19/20.
-//  Copyright © 2020 Mr Wonderful. All rights reserved.
-//
+import UIKit
 
-import Foundation
+ class QuickGrubOnboardOverlay: UIView {
+    
+     var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        return pageControl
+    }()
+    
+    open var continueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Continue", for: .normal)
+        button.contentHorizontalAlignment = .center
+        button.showsTouchWhenHighlighted = true
+        button.isHidden = true
+        button.isEnabled = false
+        return button
+    }()
+    
+    open var skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Skip", for: .normal)
+        button.contentHorizontalAlignment = .center
+        button.showsTouchWhenHighlighted = true
+        return button
+    }()
+    
+    open var prevLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Prev"
+        label.isHidden = true
+        return label
+    }()
+    
+    open var nextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Next"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+
+    
+    
+}
