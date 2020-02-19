ab65e4679e586b606c99f9dab4728bbf80c4811f
aabudu16 <Aabudu16@yahoo.com>
2020-02-19 16:01:37 -0500

    Overrides point func in QuickGrubOnboardOverlay file

--- Views/QuickGrubOnboardOverlay.swift
+++ Views/QuickGrubOnboardOverlay.swift
@@ -53,6 +53,14 @@ class QuickGrubOnboardOverlay: UIView {
         super.init(coder: aDecoder)
     }
     
+    override open func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        for subview in subviews {
+            if !subview.isHidden && subview.alpha > 0 && subview.isUserInteractionEnabled && subview.point(inside: convert(point, to: subview), with: event) {
+                return true
+            }
+        }
+        return false
+    }
     
     private func pageControlConstraint() {
         self.addSubview(pageControl)
