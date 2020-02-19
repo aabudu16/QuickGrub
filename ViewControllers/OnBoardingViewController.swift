e942ba2055e5c734d7ef4ef82ad6a5262316ab1c
aabudu16 <Aabudu16@yahoo.com>
2020-02-19 10:28:13 -0500

    Adds onBoardingVC to project

---
+++ ViewControllers/OnBoardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingViewController.swift
+//  Quick-Grub2
+//
+//  Created by Mr Wonderful on 2/19/20.
+//  Copyright © 2020 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
