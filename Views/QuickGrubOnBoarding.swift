cee30c0c308cc2c1a9e9ecf28a2f5b7382def1ad
aabudu16 <Aabudu16@yahoo.com>
2020-02-19 19:31:15 -0500

    Conforms to UIScrollViewDelegate and sets containerView.delegate = self

--- Views/QuickGrubOnBoarding.swift
+++ Views/QuickGrubOnBoarding.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class QuickGrubOnBoarding: UIView {
+class QuickGrubOnBoarding: UIView , UIScrollViewDelegate{
     
      //MARK: -- Properties
     var pageCount = 0
@@ -34,10 +34,15 @@ class QuickGrubOnBoarding: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureScrollView()
+        setScrollViewDelegate()
     }
     
     required init?(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setScrollViewDelegate(){
+        containerView.delegate = self
     }
     
     private func configureScrollView() {
