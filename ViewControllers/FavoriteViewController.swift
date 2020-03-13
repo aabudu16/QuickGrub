4da98295362f26da6c4eb8192690c5c6e7ba5f8b
aabudu16 <Aabudu16@yahoo.com>
2020-03-13 14:05:49 -0400

    Moves UITableViewDataSource extention to FavoriteViewController+UITableViewDataSource.swift file

--- ViewControllers/FavoriteViewController.swift
+++ ViewControllers/FavoriteViewController.swift
@@ -112,23 +112,3 @@ class FavoriteViewController: UIViewController {
         
     }
 }
-
-extension FavoriteViewController: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return businessFullDetail.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ResturantCellIdentifier.resturantCell.rawValue) as? FoldingCell else {return UITableViewCell()}
-        
-        cell.delegate = self
-        cell.navigateButtom.tag = indexPath.row
-        cell.moreDetailButton.tag = indexPath.row
-        cell.heartImage.tag = indexPath.row
-        let businessInfo = businessFullDetail[indexPath.row]
-        let distance = businessFullDetail[indexPath.row]
-        cell.configureBusinessData(business: businessInfo, distance: distance)
-        cell.distanceLabel.isHidden = true
-        return cell
-    }
-}
