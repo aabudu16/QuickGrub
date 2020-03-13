a90f2b8c0b8e1c6ea59bfe9e15b251b20f5e908f
aabudu16 <Aabudu16@yahoo.com>
2020-03-13 18:41:32 -0400

    Moves UITableViewDataSource extension to RestaurantResultsViewController+UITableViewDataSource.swift file

--- ViewControllers/RestaurantResultsViewController+UITableViewDataSource.swift
+++ ViewControllers/RestaurantResultsViewController+UITableViewDataSource.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 Mr Wonderful. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension RestaurantResultsViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  businessFullDetail.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ResturantCellIdentifier.resturantCell.rawValue, for: indexPath) as? FoldingCell else {return UITableViewCell()}
+        cell.delegate = self
+        cell.navigateButtom.tag = indexPath.row
+        cell.moreDetailButton.tag = indexPath.row
+        cell.heartImage.tag = indexPath.row
+        let businessInfo = businessFullDetail[indexPath.row]
+        let distance = userFoodImageSelection[indexPath.row]
+        cell.configureBusinessData(business: businessInfo, distance: distance)
+
+        return cell
+    }
+}
+
--- ViewControllers/RestaurantResultsViewController.swift
+++ ViewControllers/RestaurantResultsViewController.swift
@@ -87,22 +87,3 @@ class RestaurantResultsViewController: UIViewController {
         
     }
 }
-
-extension RestaurantResultsViewController:UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  businessFullDetail.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: ResturantCellIdentifier.resturantCell.rawValue, for: indexPath) as? FoldingCell else {return UITableViewCell()}
-        cell.delegate = self
-        cell.navigateButtom.tag = indexPath.row
-        cell.moreDetailButton.tag = indexPath.row
-        cell.heartImage.tag = indexPath.row
-        let businessInfo = businessFullDetail[indexPath.row]
-        let distance = userFoodImageSelection[indexPath.row]
-        cell.configureBusinessData(business: businessInfo, distance: distance)
-
-        return cell
-    }  
-}
