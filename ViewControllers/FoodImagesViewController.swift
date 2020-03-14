734c4d6a6e0cbf156f0fd09db9c14a6fdbc8bb72
aabudu16 <Aabudu16@yahoo.com>
2020-03-13 21:55:17 -0400

    moves CollectionViewCellDelegate extension to FoodImagesViewController+CollectionViewCellDelegate.swift file

--- ViewControllers/FoodImagesViewController.swift
+++ ViewControllers/FoodImagesViewController.swift
@@ -347,37 +347,3 @@ extension FoodImagesViewController: CLLocationManagerDelegate{
         print(error)
     }
 }
-
-extension FoodImagesViewController: CollectionViewCellDelegate{
-    func handleShortCut(tag: Int) {
-        guard let cell = collectionView.cellForItem(at: IndexPath(row: tag, section: 0)) as? FoodImagesSellectionCollectionViewCell else {return}
-        
-        cell.shortCutViewTopAnchor?.isActive = false
-        cell.newShortCutViewTopAnchor?.isActive = true
-        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            cell.shortCutView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-            cell.layoutIfNeeded()
-        }, completion: nil)
-        
-    }
-    
-    func addSelectedFood(tag: Int) {
-        let info = userCategorySelectedResults[tag]
-        guard let cell = collectionView.cellForItem(at: IndexPath(row: tag, section: 0)) as? FoodImagesSellectionCollectionViewCell else {return}
-        
-        if cell.itemIsSelected == false{
-            print(tag)
-            userFoodImageSelection.append(info)
-            print("Amount \(userFoodImageSelection.count)")
-            cell.itemIsSelected = true
-        }else{
-            print("Take care of deleting from array at that index")
-            cell.itemIsSelected = false
-            
-            
-        }
-        
-    }
-    
-    
-}
