29c3e204d4a614eb2384eacd3a12ef04fe424af4
aabudu16 <Aabudu16@yahoo.com>
2020-03-13 21:46:48 -0400

    Moves constraints to FoodImagesViewController+Constraints.swift file

--- ViewControllers/FoodImagesViewController.swift
+++ ViewControllers/FoodImagesViewController.swift
@@ -277,66 +277,6 @@ class FoodImagesViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
     }
-    
-    //MARK: Private constraints func
-    private func configureCollectionviewConstraints(){
-        view.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let pointEstimator = RelativeLayoutUtilityClass(referenceFrameSize: self.view.frame.size)
-        NSLayoutConstraint.activate([collectionView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: pointEstimator.relativeHeight(multiplier: 0.1754)), collectionView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor), collectionView.widthAnchor.constraint(equalTo: self.view.widthAnchor), collectionView.heightAnchor.constraint(equalToConstant: pointEstimator.relativeHeight(multiplier: 0.6887))])
-    }
-    
-    private func configureBackgroundImageViewConstraints(){
-        view.addSubview(backgroundImageView)
-        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([backgroundImageView.safeAreaLayoutGuide.topAnchor.constraint(equalTo: view.topAnchor), backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor), backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor), backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-    }
-    
-    private func configureDimViewConstraints(){
-        view.addSubview(dimView)
-        dimView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([dimView.topAnchor.constraint(equalTo: view.topAnchor), dimView.leadingAnchor.constraint(equalTo: view.leadingAnchor), dimView.trailingAnchor.constraint(equalTo: view.trailingAnchor), dimView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-    }
-    
-    private func configureCheckMarkIndicatorViewConstraints(){
-        self.instructionLabelView.addSubview(checkMarkIndicatorView)
-        checkMarkIndicatorView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([checkMarkIndicatorView.topAnchor.constraint(equalTo: instructionLabelView.topAnchor), checkMarkIndicatorView.leadingAnchor.constraint(equalTo: instructionLabelView.leadingAnchor), checkMarkIndicatorView.trailingAnchor.constraint(equalTo: instructionLabelView.trailingAnchor), checkMarkIndicatorView.bottomAnchor.constraint(equalTo: instructionLabel.topAnchor)])
-    }
-    
-    private func configureCheckMarkIndicatorConstraints(){
-        self.checkMarkIndicatorView.addSubview(checkMarkIndicator)
-        checkMarkIndicator.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([checkMarkIndicator.topAnchor.constraint(equalTo: checkMarkIndicatorView.topAnchor, constant: -50), checkMarkIndicator.leadingAnchor.constraint(equalTo: checkMarkIndicatorView.leadingAnchor, constant: -50), checkMarkIndicator.trailingAnchor.constraint(equalTo: checkMarkIndicatorView.trailingAnchor, constant: 50) ,checkMarkIndicator.bottomAnchor.constraint(equalTo: checkMarkIndicatorView.bottomAnchor, constant: 50)])
-    }
-    
-    private func configureInstructionLabelViewConstraints(){
-        dimView.addSubview(instructionLabelView)
-        instructionLabelView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([instructionLabelView.centerYAnchor.constraint(equalTo: dimView.centerYAnchor), instructionLabelView.centerXAnchor.constraint(equalTo: dimView.centerXAnchor), instructionLabelView.heightAnchor.constraint(equalToConstant: 300), instructionLabelView.widthAnchor.constraint(equalToConstant: 300)])
-    }
-    
-    private func configureInstructionLabelConstraints(){
-        self.instructionLabelView.addSubview(instructionLabel)
-        instructionLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([instructionLabel.leadingAnchor.constraint(equalTo: instructionLabelView.leadingAnchor), instructionLabel.trailingAnchor.constraint(equalTo: instructionLabelView.trailingAnchor) , instructionLabel.bottomAnchor.constraint(equalTo: instructionLabelView.bottomAnchor), instructionLabel.heightAnchor.constraint(equalToConstant: 200)])
-    }
-    
-    private func constraintsActivityIndicatorConstraints(){
-        self.view.addSubview(activityIndicator)
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([activityIndicator.topAnchor.constraint(equalTo: view.topAnchor),activityIndicator.leadingAnchor.constraint(equalTo: view.leadingAnchor),activityIndicator.trailingAnchor.constraint(equalTo: view.trailingAnchor),activityIndicator.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
-    }
-    
-    private func configureContinueButtomConstraints(){
-        view.addSubview(continueButtom)
-        continueButtom.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([continueButtom.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20), continueButtom.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20), continueButtom.heightAnchor.constraint(equalToConstant: 50), continueButtom.widthAnchor.constraint(equalTo: continueButtom.heightAnchor)])
-    }
 }
 
 //MARK: Extensions
